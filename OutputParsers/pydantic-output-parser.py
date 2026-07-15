0959d126ad36249d8744fddc7743e6b678786a0c
Amna Chaudhary <amnachaudhary721@gmail.com>
2026-07-15 21:44:59 +0500

    Added output parsers examples

--- OutputParsers/pydantic-output-parser.py
+++ OutputParsers/pydantic-output-parser.py
@@ -8,3 +8,18 @@
     4. Seamless Interogation (Work well with other langchain components)
     
 """
+
+
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.output_parsers import JsonOutputParser
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id = "",
+    task = "text-generation"
+)
+
+model = ChatHuggingFace(llm = llm)
