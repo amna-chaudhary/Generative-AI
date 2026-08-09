f1b4fc3514f51831d0b7e62625087a106f3a75b8
amna-chaudhary <amnachaudhary721@example.com>
2026-08-09 10:13:38 +0500

    RAG' component added

--- RAG/DocumentLoaders/webbase_loader.py
+++ RAG/DocumentLoaders/webbase_loader.py
@@ -6,3 +6,14 @@
     1. The web base loader is used to convert web scraped data into document object.
     
 """
+
+from langchain_community.document_loaders import WebBaseLoader
+
+loader = WebBaseLoader(
+    url="https://en.wikipedia.org/wiki/Generative_AI"
+)
+
+docs = loader.load()
+
+print(docs[0].page_content)
+
