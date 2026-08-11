1bd6b603de91a5028b12821ff8db707b5567ac6d
amna-chaudhary <amnachaudhary721@example.com>
2026-08-11 19:06:38 +0500

    Splitters added in RAG

---
+++ RAG/TextSplitter/documentbased_splitter.py
@@ -0,0 +1,14 @@
+"""
+    DOCUMENT-BASED SPLITTER
+
+    1. DocumentBasedSplitter is a class that splits a document into smaller chunks based on 
+    the structure of the document. 
+
+    2. It uses the RecursiveCharacterTextSplitter from the langchain_text_splitters library 
+    to perform the splitting but this time different types of (seperators) delimiters are used.
+
+    3. Example: 
+    Code is the text which we want to split into chunks. It contains classes and function so
+    we use \ndef , \nclass, etc
+     
+"""
---
+++ RAG/TextSplitter/sementicmeaning_splitter.py
@@ -0,0 +1,21 @@
+"""
+    SEMENTIC MEANING SPLITTER
+
+    1. Sementic Meaning Splitter is a tool that splits text into chunks based on semantic meaning.
+    It uses natural language processing techniques to identify the boundaries of sentences,
+      paragraphs, and other meaningful units of text.
+    
+    2. In it embedding models are used to find the sementic meaning of the text and 
+    then it is splitted into chunks based on sementic meaning.
+
+    Example:
+    There are 2 paragraphs and 1 contain 2 topic and 2nd one contain 1 topic. 
+    So it will split the text into 3 chunks based on sementic meaning by using embedding models.
+       
+        First of all, embedding models split text according to sentences and then it will find 
+        sementic meaning of each sentence and compare with previous sentence and if it is similar 
+        then it will group the sentences into chunks based on sementic meaning.
+
+
+
+"""
